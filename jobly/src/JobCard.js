1cec7d7ddc3f2fe16672ae7c01939c058510b1c8
Carl Molina <carlandrew.molina@gmail.com>
2024-02-18 12:46:16 -0800

    implemented apply button functionality

--- jobly/src/JobCard.js
+++ jobly/src/JobCard.js
@@ -1,3 +1,5 @@
+import { useContext, useState, useEffect } from "react";
+import userContext from "./userContext";
 import "./JobCard.css";
 
 /** JobCard: presentational component, shows title of job, name of company,
@@ -6,7 +8,8 @@ import "./JobCard.css";
  *  Props:
  *  job -> { title, salary, equity, companyName (optional) }
  *
- *  State: none
+ *  State:
+ *  - applied
  *
  *  JobCardList -> JobCard
  */
@@ -14,17 +17,42 @@ import "./JobCard.css";
 function JobCard({ job }) {
   console.log("JobCard input: ", job);
 
-  const { title, companyName, salary, equity } = job;
+  const { id, title, companyName, salary, equity } = job;
+
+  const { hasAppliedToJob, applyToJob } = useContext(userContext);
+  const [applied, setApplied] = useState();
+
+  useEffect(function checkApplyStatusOnMount() {
+    async function checkStatus() {
+      setApplied(hasAppliedToJob(id));
+    }
+    checkStatus();
+  }, [id, hasAppliedToJob]);
+
+  /** Applies for job */
+  async function handleApply(evt) {
+    if (hasAppliedToJob(id)) return;
+    applyToJob(id);
+    setApplied(true);
+  }
+
 
   return (
     <div className="JobCard">
       <h3 className="JobCard-title">{title}</h3>
       {companyName &&
         <p>Company: {companyName}</p>}
-      {salary &&  <p>Salary: {salary}</p>}
+      {salary && <p>Salary: {salary}</p>}
       {equity && <p>Equity: {equity}</p>}
+      <button
+        className="Jobcard-btn"
+        onClick={handleApply}
+        disabled={applied}
+      >
+        {applied ? "Applied" : "Apply"}
+      </button>
     </div>
-  )
+  );
 }
 
 
